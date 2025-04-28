b87c32df6e0171111b25c30dbc4aa52e173ba391
Andrey <semyanovandrew@yandex.ru>
2025-04-28 19:38:05 +0300

    ADD: /add_task

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from data.database import get_session
+from data.database import new_session, setup_database
 from data.bot_messages import MESSAGES
 from data.config import BOT_TOKEN
 from data.models import TaskModel
@@ -15,6 +15,7 @@ dp = Dispatcher()
 
 
 async def main():
+    # await setup_database()
     bot = Bot(token=BOT_TOKEN)
     await dp.start_polling(bot)
 
@@ -52,7 +53,7 @@ async def process_task_title(message: Message, state: FSMContext):
     title = message.text
     await state.update_data(title=title)
     await state.set_state(TaskStates.due_date)
-    await message.answer("Теперь укажите дату выполнения (ГГГГ-ММ-ДД):")
+    await message.answer("Теперь укажите дату выполнения (ДД-ММ-ГГГГ):")
 
 
 @dp.message(TaskStates.due_date)
@@ -60,24 +61,25 @@ async def process_due_date(message: Message, state: FSMContext):
     try:
         due_date = datetime.strptime(message.text, "%d-%m-%Y").date()
     except ValueError:
-        await message.answer("Неверный формат даты! Используйте ДД.ММ.ГГГГ")
+        await message.answer("Неверный формат даты! Используйте ДД-ММ-ГГГГ")
         return
 
     data = await state.get_data()
-    session = get_session()
-    try:
-        task = TaskModel(
-            user_id=message.from_user.id,
-            title=data["title"],
-            due_date=due_date,
-        )
-        session.add(task)
-        session.commit()
-        await message.answer(f"Задача добавлена!\nНазвание: {data['title']}\nСрок: {due_date}")
-    except:
-        await message.answer("Ошибка при сохранении задачи!")
-    finally:
-        await state.clear()
+    async with new_session() as session:
+        try:
+            task = TaskModel(
+                user_id=message.from_user.id,
+                title=data["title"],
+                due_date=due_date,
+            )
+            session.add(task)
+            await session.commit()
+            await message.answer(f"Задача добавлена!\nНазвание: {data['title']}\nДедлайн: {due_date}")
+        except Exception as e:
+            print(e)
+            await message.answer("Ошибка при сохранении задачи!")
+        finally:
+            await state.clear()
 
 
 if __name__ == "__main__":
