cd4b2cf971c0777eca1e6cfb2b0b2f17b4ed9307
Egor <egormoskvyak@gmail.com>
2025-05-01 11:44:50 +0300

    complete task_edit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, C
 
 from data.bot_messages import MESSAGES
 from data.config import BOT_TOKEN
-from data.database import new_session
+from data.database import new_session, setup_database
 from data.models import TaskModel, TagModel
 
 form_router = Router()
@@ -44,6 +44,9 @@ class TaskStates(StatesGroup):
     delete_number = State()
     edit_task = State()
     add_tag = State()
+    change_title = State()
+    change_deadline = State()
+    change_tag = State()
 
 
 @dp.message(Command("add_task"))
@@ -66,12 +69,19 @@ async def process_task_title_tag(message: Message, state: FSMContext):
     await message.answer("Теперь укажите дату выполнения (ДД-ММ-ГГГГ):")
 
 
-@dp.message(TaskStates.due_date)
-async def process_due_date(message: Message, state: FSMContext):
+async def date_validation(date, message):
     try:
-        due_date = datetime.strptime(message.text, "%d-%m-%Y").date()
+        due_date = datetime.strptime(date, "%d-%m-%Y").date()
+        return due_date
     except ValueError:
         await message.answer("Неверный формат даты! Используйте ДД-ММ-ГГГГ")
+        return
+
+
+@dp.message(TaskStates.due_date)
+async def process_due_date(message: Message, state: FSMContext):
+    due_date = await date_validation(message.text, message)
+    if not due_date:
         return
 
     data = await state.get_data()
@@ -144,7 +154,14 @@ async def get_tasks(status, message):
         ))
         data = list()
         for el in result.scalars().all():
-            data.append(f"{el.id}. {el.title}")
+            tag = await session.execute(sqlalchemy.select(TagModel).where(
+                TagModel.id == el.tag_id,
+            ))
+            tag = tag.scalars().first()
+            if tag:
+                data.append(f"{el.id}. {el.title} - сделать до {el.due_date}. Тег: #{tag.title}")
+            else:
+                data.append(f"{el.id}. {el.title} - сделать до {el.due_date}.")
         return data
 
 
@@ -164,93 +181,213 @@ async def get_task(data, message):
             return
 
 
-@dp.message(Command("delete_task"))
-async def delete_message(message: Message, state: FSMContext):
-    data = await get_tasks(False, message)
-    if data:
-        await message.answer(f"Выберите номер задачи для удаления:\n{'\n'.join(data)}")
-        await state.set_state(TaskStates.delete_number)
-    else:
-        await message.answer(f"Активных задач нет")
-
-
-@dp.message(TaskStates.delete_number)
-async def delete_task(message: Message, state: FSMContext):
-    data = message.text
-    task, session = await get_task(data, message)
-    answer = f"№{data}"
-    if task:
-        await session.delete(task)
-        await session.commit()
-        await message.reply(f'Задача {answer} удалена!')
-        await state.clear()
-    else:
-        await message.reply(f'Задача с {answer} не найдена.')
-
-
-@dp.message(Command("tasks"))
-async def task_buttons(message: Message):
+async def task_buttons(message, text1, text2, text3):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="Активные", callback_data="active"),
-            InlineKeyboardButton(text="Завершенные", callback_data="done"),
+            InlineKeyboardButton(text="Активные", callback_data=text1),
+            InlineKeyboardButton(text="Завершенные", callback_data=text2),
         ],
     ])
 
-    await message.answer("Выберите тип задач", reply_markup=keyboard)
+    await message.answer(text3, reply_markup=keyboard)
 
 
-@dp.callback_query(lambda c: c.data == "done")
-async def choose_done(callback: CallbackQuery):
-    data = await get_tasks(True, callback)
+async def choose_status(callback, state, arg, text1, text2):
+    data = await get_tasks(arg, callback)
     if data:
-        await callback.message.answer(f"Выполненные задачи:\n{'\n'.join(data)}")
+        await callback.message.answer(text1 + f"{'\n'.join(data)}")
+        if text1 == "Выберите номер задачи, которую хотите отредактировать:\n":
+            await state.set_state(TaskStates.edit_task)
+            if arg:
+                pass
+            else:
+                active = 1
+                await state.update_data(active=active)
     else:
-        await callback.message.answer("Завершенных задач нет")
+        await callback.message.answer(text2)
+
+
+@dp.message(Command("tasks"))
+async def tasks_buttons(message: Message):
+    await task_buttons(message, "active", "done", "Выберите тип задач")
 
 
 @dp.callback_query(lambda c: c.data == "active")
-async def choose_active(callback: CallbackQuery):
-    data = await get_tasks(False, callback)
-    if data:
-        await callback.message.answer(f"Активные задачи:\n{'\n'.join(data)}")
-    else:
-        await callback.message.answer("Активных задач нет")
+async def choose_active(callback: CallbackQuery, state: FSMContext):
+    await choose_status(callback, state, False, "Активные задачи:\n", "Активных задач нет")
+
+
+@dp.callback_query(lambda c: c.data == "done")
+async def choose_active(callback: CallbackQuery, state: FSMContext):
+    await choose_status(callback, state, True, "Завершенные задачи:\n", "Завершенных задач нет")
 
 
 @dp.message(Command("edit_task"))
-async def start_edit(message: Message, state: FSMContext):
-    data = await get_tasks(False, message)
-    if data:
-        await message.answer(f"Выберите номер задачи, которую хотите отредактировать:\n{'\n'.join(data)}")
-        await state.set_state(TaskStates.edit_task)
-    else:
-        await message.answer(f"Активных задач нет")
+async def task_edit_buttons(message: Message):
+    await task_buttons(message, "active_edit", "done_edit", "Выберите тип задачи")
+
+
+@dp.callback_query(lambda c: c.data == "done_edit")
+async def choose_done_edit(callback: CallbackQuery, state: FSMContext):
+    await choose_status(callback, state, True, "Выберите номер задачи, которую хотите отредактировать:\n",
+                        "Завершенных задач нет")
+
+
+@dp.callback_query(lambda c: c.data == "active_edit")
+async def choose_active_edit(callback: CallbackQuery, state: FSMContext):
+    await choose_status(callback, state, False, "Выберите номер задачи, которую хотите отредактировать:\n",
+                        "Активных задач нет")
 
 
 @dp.message(TaskStates.edit_task)
-async def delete(message: Message):
+async def choose_edit(message: Message, state: FSMContext):
     data = message.text
     task, session = await get_task(data, message)
     answer = f"№{data}"
+    act = await state.get_data()
+    text = "Сделать активной"
+    status = "to_active"
+    if act.get("active"):
+        text = "Завершить"
+        status = "is_done"
     if task:
         keyboard = InlineKeyboardMarkup(inline_keyboard=[
             [
-                InlineKeyboardButton(text="Удалить", callback_data="delete"),
+                InlineKeyboardButton(text="Изменить дедлайн", callback_data="change_deadline"),
                 InlineKeyboardButton(text="Изменить название", callback_data="change_text"),
             ],
             [
-                InlineKeyboardButton(text="Изменить дедлайн", callback_data="change_date"),
                 InlineKeyboardButton(text="Поменять тег", callback_data="change_tag"),
+                InlineKeyboardButton(text="Удалить", callback_data="delete"),
             ],
             [
-                InlineKeyboardButton(text="Завершить", callback_data="is_done"),
+                InlineKeyboardButton(text=text, callback_data=status),
             ],
         ])
 
+        await state.update_data(task=task, session=session, answer=answer)
         await message.answer("Выберите действие", reply_markup=keyboard)
     else:
         await message.answer(f"Задача с {answer} не найдена.")
+
+
+async def change_smth(callback, state, text, to_update):
+    data = await state.get_data()
+    task = data.get("task")
+    session = data.get("session")
+    answer = data.get("answer")
+    await callback.message.answer(text)
+    await state.set_state(to_update)
+    await state.update_data(task=task, session=session, answer=answer)
+
+
+async def get_state(state):
+    data = await state.get_data()
+    task = data.get("task")
+    session = data.get("session")
+    answer = data.get("answer")
+    return task, session, answer
+
+
+@dp.callback_query(lambda c: c.data == "delete")
+async def delete(callback: CallbackQuery, state: FSMContext):
+    task, session, answer = await get_state(state)
+    await session.delete(task)
+    await session.commit()
+    await callback.message.answer(f'Задача {answer} удалена!')
+    await state.clear()
+
+
+@dp.callback_query(lambda c: c.data == "change_text")
+async def change_text(callback: CallbackQuery, state: FSMContext, ):
+    await change_smth(callback, state, "Введите новое название", TaskStates.change_title)
+
+
+@dp.message(TaskStates.change_title)
+async def title_is_changed(message: Message, state: FSMContext):
+    new_title = message.text
+    task, session, answer = await get_state(state)
+    old_title = task.title
+    task.title = new_title
+    session.add(task)
+    await session.commit()
+    await message.answer(f'Название задачи {answer} поменяно с "{old_title}" на "{task.title}"')
+    await state.clear()
+
+
+@dp.callback_query(lambda c: c.data == "change_deadline")
+async def change_deadline(callback: CallbackQuery, state: FSMContext):
+    await change_smth(callback, state, "Введите новую дату выполнения (ДД-ММ-ГГГГ):", TaskStates.change_deadline)
+
+
+@dp.message(TaskStates.change_deadline)
+async def date_update(message: Message, state: FSMContext):
+    due_date = await date_validation(message.text, message)
+    if not due_date:
+        return
+    task, session, answer = await get_state(state)
+    old_deadline = task.due_date
+    task.due_date = due_date
+    session.add(task)
+    await session.commit()
+    await message.answer(f"Дедлайн задачи {answer} перенесён с {old_deadline} на {task.due_date}")
+    await state.clear()
+
+
+@dp.callback_query(lambda c: c.data == "change_tag")
+async def change_tag(callback: CallbackQuery, state: FSMContext):
+    await change_smth(callback, state, "Введите новый тег (#тег):", TaskStates.change_tag)
+
+
+@dp.message(TaskStates.change_tag)
+async def tag_update(message: Message, state: FSMContext):
+    title = message.text
+    task, session, answer = await get_state(state)
+    query = sqlalchemy.select(TagModel).where(TagModel.title == title)
+    tag_result = await session.execute(query)
+    tag = tag_result.scalars().first()
+    if not tag:
+        await message.answer("Тег не найден! Добавьте его через команду /add_tag")
+        return
+    new_tag = await session.execute(sqlalchemy.select(TagModel).where(
+        TagModel.title == title,
+    ))
+    new_tag = new_tag.scalars().first()
+    result = await session.execute(sqlalchemy.select(TagModel).where(
+        TagModel.id == task.tag_id,
+    ))
+    tag = result.scalars().first()
+    if tag:
+        old_tag = tag.title
+    else:
+        old_tag = None
+    task.tag_id = new_tag.id
+    session.add(task)
+    await session.commit()
+    if old_tag:
+        await message.answer(f'Тег задачи {answer} изменён с "{old_tag}" на "{title}"')
+    else:
+        await message.answer(f'К задаче {answer} добавлен тег "{title}"')
+    await state.clear()
+
+
+async def change_status(callback, state, arg, text):
+    task, session, answer = await get_state(state)
+    task.is_done = arg
+    session.add(task)
+    await session.commit()
+    await callback.message.answer(text + f" {answer}")
+    await state.clear()
+
+
+@dp.callback_query(lambda c: c.data == "is_done")
+async def date_update(callback: CallbackQuery, state: FSMContext):
+    await change_status(callback, state, True, "Вы выполнили задачу")
+
+
+@dp.callback_query(lambda c: c.data == "to_active")
+async def date_update(callback: CallbackQuery, state: FSMContext):
+    await change_status(callback, state, False, "Вы сделали активной задачу")
 
 
 if __name__ == "__main__":
